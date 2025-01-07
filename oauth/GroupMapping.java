ee0ae64b0d8935a563d7332c22d207edbb3792f4
honey1234567 <42098471+honey1234567@users.noreply.github.com>
2025-01-07 13:48:27 +0530

    Create GroupMapping.java

---
+++ oauth/GroupMapping.java
@@ -0,0 +1,24 @@
+package bravura.sonata.web.life;
+
+public class GroupMapping {
+  private String groupName;
+
+  public String getGroupName() {
+    return groupName;
+  }
+
+  public void setGroupName(String groupName) {
+    this.groupName = groupName;
+  }
+
+  public String getGroupUser() {
+    return groupUser;
+  }
+
+  public void setGroupUser(String groupUser) {
+    this.groupUser = groupUser;
+  }
+
+  private String groupUser;
+
+}
